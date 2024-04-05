50c2800d4e4b9a2a80621117f60613f58cbe35c3
Subham <shubhamrawat1843@gmail.com>
2024-04-05 22:30:05 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div><img src="https://img.freepik.com/free-vector/illustration-global-network-concept_53876-37583.jpg?w=1380&t=st=1712239073~exp=1712239673~hmac=029f70390bbd851fd58f0107a7eba607c42ea309af198817a4cfc2f0fa969992" alt="LOGO" width={'100px'} /></div>
+  )
+}
+
+export default Logo
